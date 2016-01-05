b168dff0d38914fa4ba237d5c92b990551a0615d
Rrodriguez125 <Rrodriguez125@outlook.com>
2016-01-05 11:50:04 -0800

    Signed-off-by: Rrodriguez125 <Rrodriguez125@outlook.com>

---
+++ homework/Assignment 1/Assignment 1/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.cpp
+ * Author: Ricardo Rodriguez
+ * Created on January 5, 2016, 10:18 AM
+ * Purpose: Check out IDE
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+
+int main(int argc, char** argv) {
+
+//Declare and initialize variables
+    char letter='X';
+//Input Data
+    
+    
+//calculate or map inputs to outputs
+
+    
+    
+//Output the results
+    cout<<"   "<<letter<<letter<<letter<<endl;
+    cout<<" "<<letter<<"    "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<"  "<<letter<<"   "<<letter<<endl;
+    cout<<"   "<<letter<<letter<<letter<<endl;
+//Exit stage right
+
+    return 0;
+}
+
