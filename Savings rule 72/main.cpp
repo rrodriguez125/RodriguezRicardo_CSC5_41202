5e76830b60dd90922b57659019f852e5462432e7
rrodriguez125 <rrodriguez125@outlook.com>
2016-01-21 11:50:07 -0800

    Signed-off-by: rrodriguez125 <rrodriguez125@outlook.com>

---
+++ Savings rule 72/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: Ricardo Rodriguez
+ * Created on January 4, 2016, 10:18 AM
+ * Purpose: Check out IDE
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+//the problem to solve
+cout<<"Fun look at savings and rule 72"<<endl;
+    
+//Declare and initialize variables
+float balance=100.0f;//initial balance $'s
+float intRate=.05f; //interest rate/year
+float rule72;      //rule of 72, future value =2*present value
+
+//output table heading
+cout<<"Year Year Balance Interest Earned"<<endl;
+
+//Input Data
+
+//calculate or map inputs to outputs
+
+//Output the results
+   
+    
+//Exit stage right
+
+    return 0;
+}
